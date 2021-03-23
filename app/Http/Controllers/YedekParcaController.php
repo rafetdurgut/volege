c9ae9bdbb769c8a5fb6ba7fe177b6f3d28bb11b0
ibrahim <ibrahim6551@hotmail.com>
2021-03-23 23:26:36 +0300

    Yedek parca ekle arayuz ile baglandi. Test edildi. Calisiyor. Arka planda temizleme ve iyilestirmeler yapildi.

--- app/Http/Controllers/YedekParcaController.php
+++ app/Http/Controllers/YedekParcaController.php
@@ -1,61 +1,96 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\YedekParca;
 use Illuminate\Http\Request;
 
 class YedekParcaController extends Controller
 {
-     public function ekle(){
-      $pageConfigs = ['pageHeader' => true];
+  public function ekle(Request $request)
+  {
+    $pageConfigs = ['pageHeader' => true];
 
-      $breadcrumbs = [
-        ["link" => "/", "name" => "Home"],["link" => "#", "name" => "Yedek Parça"],["name" => "Ekle"]
-      ];
-      return view('yedekparca.ekle',['pageConfigs'=>$pageConfigs,'breadcrumbs'=>$breadcrumbs]);
-    }
-    public function listele(){
-      $pageConfigs = ['pageHeader' => true];
+    $breadcrumbs = [
+      ["link" => "/", "name" => "Home"], ["link" => "#", "name" => "Yedek Parça"], ["name" => "Ekle"]
+    ];
 
-      $breadcrumbs = [
-        ["link" => "/", "name" => "Home"],["link" => "#", "name" => "Yedek Parça"],["name" => "Listele"]
-      ];
-      return view('yedekparca.listele',['pageConfigs'=>$pageConfigs,'breadcrumbs'=>$breadcrumbs]);
+    if ($request->isMethod('GET')) {
+      return view('yedekparca.ekle', ['pageConfigs' => $pageConfigs, 'breadcrumbs' => $breadcrumbs]);
     }
 
-      public function stoknoyedekparca(Request $request)
-      {
-        
-          $search = $request->search;
-          $parcalar =  array();
-          if($search != ''){
-              $parcalar = YedekParca::orderby('stokkodu','asc')->where('stokkodu', 'like', '%' .$search . '%')->limit(5)->get();
-          }
-    
-          $response = array();
-          foreach($parcalar as $parca){
-              $response[] = array("label"=>$parca->stokkodu,"value"=>$parca);
-          }
-          return $response;    
-      }
-      public function stokadyedekparca(Request $request)
-      {
-        
-          $search = $request->search;
-          $parcalar =  array();
-          if($search != ''){
-              $parcalar = YedekParca::orderby('stokadi','asc')->where('stokadi', 'like', '%' .$search . '%')->limit(5)->get();
-          }
-    
-          $response = array();
-          foreach($parcalar as $parca){
-            if(isset($parca->ureticikodu))
-              $response[] = array("label"=>$parca->stokadi ."(".$parca->ureticikodu.")","value"=>$parca);
-            else
-              $response[] = array("label"=>$parca->stokadi,"value"=>$parca);
-          }
-          return $response;    
-      }
+    if ($request->isMethod('POST')) {
+
+      $request->validate(
+        [
+          'stokkodu' => 'required',
+          'stokadi' => 'required',
+          'kdv' => 'required | min:1 | max:100',
+        ]
+      );
+
+      //veriyi ekle
+      $yedekparcabilgi = new YedekParca();
+      $yedekparcabilgi->ureticikodu = $request->ureticikodu; //input('ureticikodu');
+      $yedekparcabilgi->stokadi = $request->stokadi; //input('stokadi');
+      $yedekparcabilgi->stokkodu = $request->stokkodu; //input('stokkodu');
+      $yedekparcabilgi->barkod = $request->barkod; //input('barkod');
+      $yedekparcabilgi->urungrup = $request->grup; //input('grup');
+      $yedekparcabilgi->birim = $request->birim;
+      $yedekparcabilgi->alisfiyati = $request->alisfiyati;
+      $yedekparcabilgi->stokadet = $request->stokadet;
+      $yedekparcabilgi->kdv = $request->kdv;
+      $yedekparcabilgi->satisfiyati = $request->satisfiyati;
+      $yedekparcabilgi->uyarimiktari = $request->uyarimiktari;
+      $yedekparcabilgi->created_at = date("Y-m-d H:i:s");
+      //insert
+      $yedekparcabilgi->save();
+      return redirect()->route('yedekparca-ekle')
+        ->with('success', 'Yedek Parça Başarıyla Eklendi.');
+    }
+  }
+
+  public function listele()
+  {
+    $pageConfigs = ['pageHeader' => true];
+
+    $breadcrumbs = [
+      ["link" => "/", "name" => "Home"], ["link" => "#", "name" => "Yedek Parça"], ["name" => "Listele"]
+    ];
+    return view('yedekparca.listele', ['pageConfigs' => $pageConfigs, 'breadcrumbs' => $breadcrumbs]);
+  }
+
+  public function stoknoyedekparca(Request $request)
+  {
+
+    $search = $request->search;
+    $parcalar =  array();
+    if ($search != '') {
+      $parcalar = YedekParca::orderby('stokkodu', 'asc')->where('stokkodu', 'like', '%' . $search . '%')->limit(5)->get();
+    }
+
+    $response = array();
+    foreach ($parcalar as $parca) {
+      $response[] = array("label" => $parca->stokkodu, "value" => $parca);
+    }
+    return $response;
+  }
+  public function stokadyedekparca(Request $request)
+  {
+
+    $search = $request->search;
+    $parcalar =  array();
+    if ($search != '') {
+      $parcalar = YedekParca::orderby('stokadi', 'asc')->where('stokadi', 'like', '%' . $search . '%')->limit(5)->get();
+    }
+
+    $response = array();
+    foreach ($parcalar as $parca) {
+      if (isset($parca->ureticikodu))
+        $response[] = array("label" => $parca->stokadi . "(" . $parca->ureticikodu . ")", "value" => $parca);
+      else
+        $response[] = array("label" => $parca->stokadi, "value" => $parca);
+    }
+    return $response;
+  }
 }
-
-
--- resources/views/yedekparca/ekle.blade.php
+++ resources/views/yedekparca/ekle.blade.php
@@ -1,9 +1,29 @@
 @extends('layouts.contentLayoutMaster')
 {{-- page title --}}
 @section('title','Yedek Parça Ekle')
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+{{-- page style --}}
+@section('page-styles')
+<link rel="stylesheet" type="text/css" href="{{asset('css/plugins/forms/wizard.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.css" integrity="sha512-uq8QcHBpT8VQcWfwrVcH/n/B6ELDwKAdX4S/I3rYSwYldLVTs7iII2p6ieGCM13QTPEKZvItaNKBin9/3cjPAg==" crossorigin="anonymous" />
+@endsection
 
 @section('content')
+<form method="POST" action="{{route('yedekparca-ekle')}}" >
+    @csrf
 <div class="bg-white p-3 shadow">
+    @if(Session::get('success'))
+        <div class="alert alert-success">
+            {{ Session('success')}}
+        </div>
+        @endif
+        @if(Session::get('error'))
+        <div class="alert alert-dange">
+            {{ Session('error')}}
+        </div>
+    @endif
   <legend> Yedek Parça Bilgileri </legend>
   <div class="form-group row">
       <div class="col-sm-6">
@@ -30,7 +50,7 @@
               <label for="alisfiyati" class="col-sm-3 col-form-label">Alış Fiyatı</label>
               <div class="input-group col-sm-9">
                       <span class="input-group-text" id="basic-addon1">TL</span>
-                      <input id="alisfiyati" name="alisfiyati" type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                      <input id="alisfiyati" name="alisfiyati" type="text" class="form-control" placeholder="">
               </div>
           </div>
 
@@ -38,7 +58,7 @@
               <label for="kdv" class="col-sm-3 col-form-label">KDV</label>
               <div class="input-group col-sm-9">
                       <span class="input-group-text" id="basic-addon1">%</span>
-                      <input id="kdv" name="kdv" type="number" class="form-control" value="18" aria-label="Username" aria-describedby="basic-addon1">
+                      <input id="kdv" name="kdv" type="number" class="form-control" value="18">
               </div>
           </div>
 
@@ -54,26 +74,26 @@
           <div class="form-group row">
               <label for="stokadi" class="col-sm-3 col-form-label">Stok Adı:</label>
               <div class="col-sm-9">
-              <input id="stokadi" name="stokadi" type="text" class="form-control" id="telefon" placeholder="">
+              <input id="stokadi" name="stokadi" type="text" class="form-control" required placeholder="">
               </div>
           </div>
 
           <div class="form-group row">
               <label for="barkod" class="col-sm-3 col-form-label">Barkod:</label>
               <div class="col-sm-9">
-              <input name="barkod" id="barkod" type="password" class="form-control" id="vergino" placeholder="">
+              <input name="barkod" id="barkod" type="text" class="form-control" placeholder="">
               </div>
           </div>
 
           <div class="form-group row">
               <label for="birim" class="col-sm-3 col-form-label">Birim:</label>
               <div class="col-sm-9">
-                  <select class="form-control" id="birim" aria-label="Default select example">
+                  <select class="form-control" id="birim" name="birim" aria-label="Default select example">
                       <option value="1">Birim Yok</option>
-                      <option value="1" selected>Adet</option>
-                      <option value="2">Litre</option>
-                      <option value="3">Takım</option>
-                      <option value="4">Paket</option>
+                      <option value="2" selected>Adet</option>
+                      <option value="3">Litre</option>
+                      <option value="4">Takım</option>
+                      <option value="5">Paket</option>
                     </select>
               </div>
           </div>
@@ -87,7 +107,7 @@
               <label for="satisfiyati" class="col-sm-3 col-form-label">Satış Fiyatı:</label>
               <div class="input-group col-sm-9">
                       <span class="input-group-text" id="basic-addon1">TL</span>
-                      <input type="text" class="form-control" id="satisfiyati" name="satisfiyati" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                      <input type="text" class="form-control" id="satisfiyati" name="satisfiyati" placeholder="">
               </div>
           </div>
       </div>
@@ -98,4 +118,19 @@
   </div>
 </div>
 </form>
+@endsection
+
+{{-- vendor scripts --}}
+@section('vendor-scripts')
+<script src="{{asset('vendors/js/extensions/jquery.steps.min.js')}}"></script>
+<script src="{{asset('vendors/js/forms/validation/jquery.validate.min.js')}}"></script>
+@endsection
+
+{{-- page scripts --}}
+@section('page-scripts')
+<script src="{{asset('js/scripts/forms/wizard-steps.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-autocomplete/1.0.7/jquery.auto-complete.min.js" integrity="sha512-TToQDr91fBeG4RE5RjMl/tqNAo35hSRR4cbIFasiV2AAMQ6yKXXYhdSdEpUcRE6bqsTiB+FPLPls4ZAFMoK5WA==" crossorigin="anonymous"></script>
+<script src="{{asset('js/scripts/autocomplete/autocomplete.js')}}"></script>
+
 @endsection
